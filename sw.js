cdfd17ec3ad965d6c49ca6e949a635c0770c8105
Agenilson Gomes da Silva <SEU_EMAIL_DO_GITHUB>
2026-09-13 16:05:14 -0300

    feat: finaliza ajustes visuais e integra SIVISA na Central

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
    ============================================================= */
 'use strict';
 
-const VERSAO = '20260913-4';
+const VERSAO = '20260913-5';
 const CACHE_APP   = 'app-' + VERSAO;
 const CACHE_DADOS = 'dados-v1';
 
