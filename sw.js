1c3b7017723a38e439afc2a04d99961abfac7a93
Farmacia <92679083+uvisvp@users.noreply.github.com>
2026-09-14 23:39:34 -0300

    Atualiza cache para Central AFE/AE schema 3

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
    ============================================================= */
 'use strict';
 
-const VERSAO = '20260913-17';
+const VERSAO = '20260914-18';
 const CACHE_APP   = 'app-' + VERSAO;
 const CACHE_DADOS = 'dados-v1';
 
